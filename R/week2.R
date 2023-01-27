0b608207a8fed805bbb8c14c54750f56e16644c6
Joy Zhou <zhou1559@umn.edu>
2023-01-26 20:28:13 -0600

    Labeled condition and gender variables

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,4 @@
 #Import and Labeling
 rt_df <- read.csv("data/week2.csv", header = TRUE)
+rt_df$condition <- factor(rt_df$condition, ordered = FALSE, levels = c("A", "B"),labels = c("Control", "Experimental"))
+rt_df$gender<- factor(rt_df$gender, ordered = FALSE, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
